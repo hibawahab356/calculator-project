e693e16be1b8c3baf55cddcc15615f1cddf59eba
hibawahab356 <ranihiba356@gmail.com>
2026-07-03 21:03:55 +0500

    calculator project

---
+++ app.js
@@ -0,0 +1,35 @@
+let btns = document.querySelectorAll(".box")
+let userInput = document.querySelector("#data")
+
+
+
+for(let i = 0;i<btns.length; i++) {
+btns[i].addEventListener("click",(event)=>{
+  
+userInput.value += event.target.innerText
+
+
+
+})
+    }
+
+
+
+    // result 
+
+
+    let result = document.querySelector("#res")
+    result.addEventListener("click" ,()=>{
+      
+        let output = (eval(userInput.value))
+        userInput.value = output;
+   
+
+    })
+
+    // clear
+
+    let clear = document.querySelector("#btn")
+    btn.addEventListener("click" ,()=>{
+      userInput.value = " "
+    })
